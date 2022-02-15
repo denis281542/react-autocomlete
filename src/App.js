d2f61d194575c6dd6ea01a4bbef8b889e80a2257
Denis Kuimov <deniskuimov@mail.ru>
2022-02-15 21:51:31 +0300

    rename and edit inputFetch

--- src/App.js
+++ src/App.js
@@ -1,117 +1,116 @@
 import './App.css';
 import {useState, useEffect} from 'react'
 import {InputFetch} from './InputFetch'
-import {InputFetchNames} from './InputFetchNames'
 
 function App() {
-  const [street, setStreet] = useState('')
-  const [streets, setStreets] = useState([])
-  const [id, setId] = useState('')
-  const [addressId, setAddressId] = useState('')
-  const [houseId, setHouseId] = useState('')
-  const [house, setHouse] = useState('')
-  const [houses, setHouses] = useState([])
-  const [flat, setFlat] = useState('')
-  const [flats, setFlats] = useState([])
-  const [phone, setPhone] = useState('')
-  // const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [clientId, setClientId] = useState(0)
-  const [clients, setClients] = useState([])
+  // const [street, setStreet] = useState('')
+  // const [streets, setStreets] = useState([])
+  // const [id, setId] = useState('')
+  // const [addressId, setAddressId] = useState('')
+  // const [houseId, setHouseId] = useState('')
+  // const [house, setHouse] = useState('')
+  // const [houses, setHouses] = useState([])
+  // const [flat, setFlat] = useState('')
+  // const [flats, setFlats] = useState([])
+  // const [phone, setPhone] = useState('')
+  // // const [name, setName] = useState('')
+  // const [email, setEmail] = useState('')
+  // const [clientId, setClientId] = useState(0)
+  // const [clients, setClients] = useState([])
 
-  const user = {
-    Id: 0,
-    // Name: name,
-    Phone: phone,
-    Email: email,
-    BindId: 0
-  }
+  // const user = {
+  //   Id: 0,
+  //   // Name: name,
+  //   Phone: phone,
+  //   Email: email,
+  //   BindId: 0
+  // }
   
-  const searchStreet = async (text) => {
-    const res = await fetch(`https://dispex.org/api/vtest/Request/streets`)
-    const streets = await res.json();
-    setStreets(streets);
-  }
+  // const searchStreet = async (text) => {
+  //   const res = await fetch(`https://dispex.org/api/vtest/Request/streets`)
+  //   const streets = await res.json();
+  //   setStreets(streets);
+  // }
 
-  const searchHouse = async (id) => {
-    const res = await fetch(`https://dispex.org/api/vtest/Request/houses/${id}`)
-    const houses = await res.json();
-    setHouses(houses);
-  }
+  // const searchHouse = async (id) => {
+  //   const res = await fetch(`https://dispex.org/api/vtest/Request/houses/${id}`)
+  //   const houses = await res.json();
+  //   setHouses(houses);
+  // }
   
-  const filteredStreet = streets.filter(s => s.name.substring(0, street.length).toLowerCase() === street.toLowerCase())
-  const filteredHouse = houses.filter(h => h.name.substring(0, house.length).toLowerCase() === house.toLowerCase())
-  const filteredFlat = flats.filter(h => h.name.substring(0, flat.length).toLowerCase() === flat.toLowerCase())
+  // const filteredStreet = streets.filter(s => s.name.substring(0, street.length).toLowerCase() === street.toLowerCase())
+  // const filteredHouse = houses.filter(h => h.name.substring(0, house.length).toLowerCase() === house.toLowerCase())
+  // const filteredFlat = flats.filter(h => h.name.substring(0, flat.length).toLowerCase() === flat.toLowerCase())
     
-  const searchFlat = async (id) => {
-    const res = await fetch(`https://dispex.org/api/vtest/Request/house_flats/${id}`)
-    const flats = await res.json();
-    setFlats(flats);
-  }
+  // const searchFlat = async (id) => {
+  //   const res = await fetch(`https://dispex.org/api/vtest/Request/house_flats/${id}`)
+  //   const flats = await res.json();
+  //   setFlats(flats);
+  // }
 
-  const addClient = async () => {
-    const response = await fetch('https://dispex.org/api/vtest/HousingStock/client', {
-      method: 'POST', 
-      mode: 'cors',
-      cache: 'no-cache', 
-      credentials: 'same-origin', 
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer', 
-      body: JSON.stringify(user) 
-    });
-    return console.log(response)
-  }
+  // const addClient = async () => {
+  //   const response = await fetch('https://dispex.org/api/vtest/HousingStock/client', {
+  //     method: 'POST', 
+  //     mode: 'cors',
+  //     cache: 'no-cache', 
+  //     credentials: 'same-origin', 
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     redirect: 'follow',
+  //     referrerPolicy: 'no-referrer', 
+  //     body: JSON.stringify(user) 
+  //   });
+  //   return console.log(response)
+  // }
 
-  const bindClient = async () => {
-    const res = await fetch(`https://dispex.org/api/vtest/HousingStock/client?phone=${phone}`)
-    const client = await res.json()
-    setClientId(client.id);
+  // const bindClient = async () => {
+  //   const res = await fetch(`https://dispex.org/api/vtest/HousingStock/client?phone=${phone}`)
+  //   const client = await res.json()
+  //   setClientId(client.id);
 
-    const response = await fetch('https://dispex.org/api/vtest/HousingStock/bind_client', {
-      method: 'PUT', 
-      mode: 'cors',
-      cache: 'no-cache', 
-      credentials: 'same-origin', 
-      headers: {
-        // 'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-      },
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer', 
-      body: JSON.stringify({
-        AddressId: addressId,
-        ClientId: client.id
-      })
-    });
-    return await response.json() 
-  }
+  //   const response = await fetch('https://dispex.org/api/vtest/HousingStock/bind_client', {
+  //     method: 'PUT', 
+  //     mode: 'cors',
+  //     cache: 'no-cache', 
+  //     credentials: 'same-origin', 
+  //     headers: {
+  //       // 'Access-Control-Allow-Origin': '*',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     redirect: 'follow',
+  //     referrerPolicy: 'no-referrer', 
+  //     body: JSON.stringify({
+  //       AddressId: addressId,
+  //       ClientId: client.id
+  //     })
+  //   });
+  //   return await response.json() 
+  // }
 
-  const getClients = async () => {
-    const response = await fetch(`https://dispex.org/api/vtest/HousingStock/clients?addressId=${addressId}`);
-    const clients = await response.json()
-    setClients(clients)
-    return console.log(clients);
-  }
+  // const getClients = async () => {
+  //   const response = await fetch(`https://dispex.org/api/vtest/HousingStock/clients?addressId=${addressId}`);
+  //   const clients = await response.json()
+  //   setClients(clients)
+  //   return console.log(clients);
+  // }
 
-  const deleteClient = async bindId => {
-    const res = await fetch(`https://dispex.org/api/vtest/HousingStock/bind_client/${bindId}`, {
-      method: 'DELETE'
-    });
-  }
+  // const deleteClient = async bindId => {
+  //   const res = await fetch(`https://dispex.org/api/vtest/HousingStock/bind_client/${bindId}`, {
+  //     method: 'DELETE'
+  //   });
+  // }
 
-  let flatClients = clients.map(client => {
-    return(
-      <div key={client.id}>
-        <div>Имя: {client.name}</div>
-        <div>Телефон: {client.phone}</div>
-        <div>Email: {client.email}</div>
-        <button onClick={() => deleteClient(client.bindId)}>Удалить жильца</button>
-      </div>
-    )
-  })
+  // let flatClients = clients.map(client => {
+  //   return(
+  //     <div key={client.id}>
+  //       <div>Имя: {client.name}</div>
+  //       <div>Телефон: {client.phone}</div>
+  //       <div>Email: {client.email}</div>
+  //       <button onClick={() => deleteClient(client.bindId)}>Удалить жильца</button>
+  //     </div>
+  //   )
+  // })
 
   
  
@@ -126,11 +125,19 @@ function App() {
 const [names, setNames] = useState([])
 const [name, setName] = useState('')
 const [fetchNames, setFetchNames] = useState([])
+const [filterCities, setFilterCities] = useState([])
+const [city, setCity] = useState('')
+const [subject, setSubject] = useState('')
+const [cities, setCities] = useState([])
 
 useEffect(() => {
   fetch('https://firebasestorage.googleapis.com/v0/b/megalandpark.appspot.com/o/russian_names.json?alt=media&token=33f85cec-a3be-4a65-8e98-25be7c9a6d67')
     .then(response => response.json())
     .then(json => setNames(json))
+
+  fetch('https://firebasestorage.googleapis.com/v0/b/megalandpark.appspot.com/o/cities.json?alt=media&token=452c6222-c43a-4a57-81a3-2bc148b527d2')
+    .then(response => response.json())
+    .then(json => setCities(json))
 
   document.body.addEventListener('click', closeAutocomplete);
   document.addEventListener("keydown", escFunction);
@@ -144,18 +151,15 @@ useEffect(() => {
 const closeAutocomplete = e => {
   if(!e.target.closest('li')) {
       setFetchNames([])
+      setFilterCities([])
   }
 }  
 
 const escFunction = (e) => {
   if (e.key === "Escape") {
     setFetchNames([])
+    setFilterCities([])
   }
-}
-
-const onChange = e => {
-  setName(e.target.value)
-  setFetchNames(names.filter(n => n.Name.toLowerCase().includes(e.target.value)))
 }
 
 let filteredList
@@ -174,15 +178,49 @@ let filteredList
     })
   }
 
+  const filterCity = filterCities.map(c => {
+    return(
+      <li 
+      className="autocomplete__item"
+      key={c.coords.lat + c.coords.lon}
+      onClick={() => {
+          setCity(c.name)
+          setFilterCities([])
+          setSubject(c.subject)
+      }}
+      >{c.name} <small>{c.subject}</small></li>
+    )
+  })
+
+  const onChange = e => {
+    setName(e.target.value)
+    setFetchNames(names.filter(n => n.Name.toLowerCase().includes(e.target.value)))
+  }
+
+  const onChangeCity = e => {
+    setCity(e.target.value)
+    setFilterCities(cities.filter(c => c.name.toLowerCase().includes(e.target.value)))
+  }
+
   return (
     <div className="card">
-      <InputFetch />
-      <InputFetchNames
+      <InputFetch
+        label='Город'
+        htmlFor='city'
+        id='city'
+        text='text'
+        placeholder='Введите город'
+        filteredList={filterCity}
+        value={city}
+        onChange={onChangeCity}
+      />
+
+      <InputFetch
         label='Имя'
         htmlFor='name'
         id='name'
         text='text'
-        placeholder='Введите ФИО'
+        placeholder='Введите имя'
         filteredList={filteredList}
         value={name}
         onChange={onChange}
--- src/InputFetch.js
+++ src/InputFetch.js
@@ -1,73 +1,20 @@
-import { useState, useEffect } from "react"
-
-export const InputFetch = () => {
-    const [subject, setSubject] = useState('')
-    const [city, setCity] = useState('')
-    const [filterCities, setFilterCities] = useState([])
-    const [cities, setCities] = useState([])
-
-    useEffect(() => {    
-        fetch('https://firebasestorage.googleapis.com/v0/b/megalandpark.appspot.com/o/cities.json?alt=media&token=452c6222-c43a-4a57-81a3-2bc148b527d2')
-            .then(response => response.json())
-            .then(json => setCities(json))
-
-        document.body.addEventListener('click', closeAutocomplete);
-        document.addEventListener('keydown', escFunction);
-
-        return () => {
-            document.body.removeEventListener('click', closeAutocomplete);
-            document.removeEventListener('keydown', escFunction);
-        }
-    }, []) 
-    // useEffect(() => {    
-    //     
-    // }, []) 
-
-    const closeAutocomplete = e => {
-        if(!e.target.closest('li')) {
-            setFilterCities([])
-        }
-    }
-
-    const escFunction = (e) => {
-        if (e.key === "Escape") {
-            setFilterCities([])
-        }
-    }
-
-    const filterCity = filterCities.map(c => {
-        return(
-            <li 
-            className="autocomplete__item"
-            key={c.coords.lat + c.coords.lon}
-            onClick={() => {
-                setCity(c.name)
-                setFilterCities([])
-                setSubject(c.subject)
-            }}
-            >{c.name} <small>{c.subject}</small></li>
-        )
-    })
-    
+export const InputFetch = ({label, htmlFor, id, text, placeholder, filteredList, value, onChange, subject}) => {
 
     return(
         <div className="city">
-        {subject}
-        <label htmlFor="city">Город</label>
-        <input 
-          id='city' 
-          type="text" 
-          placeholder="Введите город" 
-          autoComplete="off"
-          onChange={e => {
-            setCity(e.target.value)
-            setFilterCities(cities.filter(c => c.name.toLowerCase().includes(e.target.value)))
-          }}
-          value={city}
-        /> 
-        <ul className="autocomplete__list"> 
-          {filterCity}
-        </ul>      
-      </div>
+            {subject || ''}
+            <label htmlFor={htmlFor}>{label}</label>
+            <input 
+            id={id} 
+            type={text}
+            placeholder={placeholder} 
+            onChange={onChange}
+            value={value}
+            autoComplete="off"
+            /> 
+            <ul className="autocomplete__list">
+                {filteredList}
+            </ul>      
+        </div>
     )
 }
