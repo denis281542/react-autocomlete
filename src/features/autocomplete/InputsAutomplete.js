ac2436d708c0c69446f710175c48d919a531879e
Denis Kuimov <deniskuimov@mail.ru>
2022-03-11 13:59:27 +0300

    fix bug autocomplete

--- src/features/autocomplete/InputsAutomplete.js
+++ src/features/autocomplete/InputsAutomplete.js
@@ -57,7 +57,7 @@ export const InputsAutomplete = () => {
                     onChange={e => house.length > e.target.value.length ? dispatch(clearUsers()) : null}
                     label="Дом" 
                 />}
-            />
+            /> 
             <Autocomplete
                 disablePortal
                 noOptionsText='Совпадения не найдены'
